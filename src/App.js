a635591c46b29ca72d6422eb1d7983ed61344464
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2020-12-23 23:57:25 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import "./App.css";
+import React, { useState } from "react";
 import Header from "./componets/Header";
 import Sidebar from "./componets/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./componets/Chat";
+import Login from "./componets/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <Chat />
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
---
+++ src/componets/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img src="../Slack_RGB.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+// 3;05
